59d7cf9e5a4616ab07c7d51bd6d165103b3a018d
12ahmed52 <ahmedashrafhassan009@gmail.com>
2023-10-23 10:51:06 +0300

    min commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <numeric> // for std::accumulate
+#include <algorithm>
 
 int getSum(const std::vector<int>& vec) {
     return std::accumulate(vec.begin(), vec.end(), 0);
 }
 
+int getMin(const std::vector<int>& vec) {
+    return *std::min_element(vec.begin(), vec.end());
+}
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(numbers);
+    int min = getMin(numbers);
     std::cout << "Sum of numbers: " << sum << std::endl;
+    std::cout << "Minimum of numbers: " << min << std::endl;
     return 0;
 }
