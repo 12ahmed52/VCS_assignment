62c5d90d67065c67f1f89cf4ca495b8a5681e23e
12ahmed52 <ahmedashrafhassan009@gmail.com>
2023-10-23 10:49:05 +0300

    average commit

--- main.cpp
+++ main.cpp
@@ -6,9 +6,16 @@ int getSum(const std::vector<int>& vec) {
     return std::accumulate(vec.begin(), vec.end(), 0);
 }
 
+double getAverage(const std::vector<int>& vec) {
+    int sum = getSum(vec);
+    return static_cast<double>(sum) / vec.size();
+}
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(numbers);
+    double average = getAverage(numbers);
     std::cout << "Sum of numbers: " << sum << std::endl;
+    std::cout << "Average of numbers: " << average << std::endl;
     return 0;
 }
